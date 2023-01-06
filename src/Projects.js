92ce4afd55521926a44b46fceccecf9c1a795fe5
Omar Attaalla <oatta001@ucr.edu>
2023-01-06 11:13:13 -0800

    Add Ball Blaster and InvoiceManager to Projects

--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,8 @@ import ChessImage from "./images/Chess.png"
 import SortImage from "./images/Sort.png"
 import RoBooksImage from "./images/RoBooks.png"
 import ReceiptSimple from "./images/ReceiptSimpleDemo.webp"
+import InvoiceImage from "./images/invoiceManager.png"
+import BallBlastImage from "./images/BallBlast.png"
 
 export default function Projects() {
 
@@ -23,6 +25,16 @@ export default function Projects() {
             </div>
             <div className="BoxContainer">
                 <div className="BoxTitle">
+                    <a className = "ProjectLink" href="https://github.com/OmarAttaalla/invoiceManager" target={"_blank"} rel="noopener noreferrer">Invoice Manager</a>
+                    <ul className="SubInfo">
+                        <li>Invoice Manager is an application that allows users to organize their individual projects. Information is stored about each project and allows the user to generate invoices automatically.</li>
+                        <li>This app was made using Python and PySimpleGUI</li>
+                    </ul>
+                </div>
+                <img id="InvoiceImage" src={InvoiceImage} alt=""></img>
+            </div>
+            <div className="BoxContainer">
+                <div className="BoxTitle">
                     <a href="https://github.com/OmarAttaalla/Chess-Game" target={"_blank"} rel="noopener noreferrer">Chess Game</a>
                     <br/>
                     <ul className="SubInfo">
@@ -33,6 +45,18 @@ export default function Projects() {
                 </div>
                 <img id="ChessImage" src={ChessImage} alt=""></img>
             </div>
+
+            <div className="BoxContainer">
+                <div className="BoxTitle">
+                    <a className = "ProjectLink" href="https://github.com/OmarAttaalla/AI-Learns-Ball-Blast" target={"_blank"} rel="noopener noreferrer">AI Learns Ball Blast</a>
+                    <ul className="SubInfo">
+                        <li>AI Learns Ball Blast is a multi-step project which includes a game (Ball Blast) as well as an AI that uses Deep Reinforcement learning to improve it's ability to play the game.</li>
+                        <li>This app was made using Python, PyGame, and TensorFlow.</li>
+                    </ul>
+                </div>
+                <img id="BallBlastImage" src={BallBlastImage} alt=""></img>
+            </div>
+
             <div className="BoxContainer">
                 <div className="BoxTitle">
                     <a href="./Sorting.html" target={"_blank"} rel="noopener noreferrer">Visual Sorting Algorithms</a>
