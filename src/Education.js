77240b501141d118217195631e01c2214ba84bfb
Omar Attaalla <oatta001@ucr.edu>
2023-02-02 13:02:15 -0800

    Change to Computer Science & Engineering

--- src/Education.js
+++ src/Education.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Education.css"
 import UCRImage from './images/UCR.png'
-import UCIImage from './images/UCI.png'
+import UCIImage from './images/UCIICS.png'
 
 export default function Education() {
 
@@ -9,14 +9,18 @@ export default function Education() {
         <>
             <div className="Title">Education</div>
             <div className="BoxContainer">
-                <img className="UCRImage" src={UCIImage} alt=""></img>
+                <div className="UCIImageContainer">
+                    <img className="UCIImage" src={UCIImage} alt=""></img>
+                </div>
                 <div className="BoxTitle">
                     University of California, Irvine
                     <br/>
-                    <li className="CompEng">B.S. Computer Engineering</li>
+                    <li className="CompEng">B.S. Computer Science & Engineering</li>
                     <ul className="SubInfo">
                         <li>Data Structures and Algorithms</li>
                         <li className="subSubInfo">Arrays, Stacks, Queues, Heaps, Hash Tables, Trees, Graphs</li>
+                        <li>Introduction to Data Management</li>
+                        <li className="subSubInfo">E/R Data Model, Relation Data Model/Design Theory, Relation Algebra/Calculus, SQL, DB Design</li>
                     </ul>
                 </div>
             </div>
